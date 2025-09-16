bd473f3c22b8ca29649d46ba19399c6ffc3e97ac
ilbertt <luca.bertelli@dfinity.org>
2025-09-16 11:06:08 +0200

    feat: add example for icp features config

---
+++ examples/icp_features/tests/src/icp-features.spec.ts
@@ -0,0 +1,49 @@
+import { resolve } from 'path';
+import { Actor, IcpFeaturesConfig, PocketIc } from '@dfinity/pic';
+import { type _SERVICE, idlFactory } from '../../declarations/icp_features.did';
+import { Principal } from '@dfinity/principal';
+
+const WASM_PATH = resolve(
+  __dirname,
+  '..',
+  '..',
+  '..',
+  '..',
+  '.dfx',
+  'local',
+  'canisters',
+  'icp_features',
+  'icp_features.wasm.gz',
+);
+
+const E8S_PER_ICP = 100_000_000;
+
+describe('ICP Features', () => {
+  let pic: PocketIc;
+  let actor: Actor<_SERVICE>;
+
+  beforeEach(async () => {
+    pic = await PocketIc.create(process.env.PIC_URL, {
+      icpFeatures: {
+        icpToken: IcpFeaturesConfig.DefaultConfig,
+        // you can also enable other ICP features here
+      },
+    });
+
+    const fixture = await pic.setupCanister<_SERVICE>({
+      idlFactory,
+      wasm: WASM_PATH,
+    });
+    actor = fixture.actor;
+  });
+
+  afterEach(async () => {
+    await pic.tearDown();
+  });
+
+  it('ICP ledger should be available (get balance)', async () => {
+    // Pocket IC sets 1B ICP as the initial balance for the anonymous principal
+    const balance = await actor.get_balance(Principal.anonymous());
+    expect(balance).toBe(BigInt(1_000_000_000) * BigInt(E8S_PER_ICP));
+  });
+});
