be8c10fa42220ee81d4788178da41186bebb473b
ilbertt <luca.bertelli@dfinity.org>
2025-09-19 15:52:14 +0200

    docs: use `@dfinity/typedoc-plugin-icp-docs` package

--- docs/scripts/utils/constants.ts
+++
@@ -1,5 +0,0 @@
-import path from 'node:path';
-
-export const CONTENT_DIR = path.join('src', 'content');
-export const TMP_DIR = path.join(CONTENT_DIR, 'tmp');
-export const DOCS_DIR = path.join(CONTENT_DIR, 'docs');
